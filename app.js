7254e815ca10b5ef964dec5a1432c437a5f0a62a
Aviral Srivastava <srivastavaaviral1@gmail.com>
2017-08-09 20:09:20 +0530

    final

---
+++ app.js
@@ -0,0 +1,104 @@
+
+
+$(".fa-bar-chart").click(function(){
+
+$(this).toggleClass("active");
+if(equal==0)
+{
+
+equal=1;
+
+$("svg").css("display","inline-block");
+$(".content").css("display","none");
+$(".contain").css("display","inline-block");
+$(".contain").css("background","black");
+
+
+}
+else{
+equal=0;
+$("svg").css("display","none");
+$(".content").css("display","inline-block");
+$(".contain").css("display","none");
+
+
+
+
+}
+
+
+
+
+
+
+
+})
+
+
+$(document).ready(function () {
+	
+	
+
+  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  var audioElement = document.querySelector('audio');
+  var audioSrc = audioCtx.createMediaElementSource(audioElement);
+  var analyser = audioCtx.createAnalyser();
+
+  // Bind our analyser to the media element source.
+  audioSrc.connect(analyser);
+  audioSrc.connect(audioCtx.destination);
+  audioSrc.crossOrigin = "anonymous";
+
+  //var frequen
+
+  var svgHeight = '300';
+  var svgWidth = '1300'.crossOrigin = "anonymous";
+
+  //var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+  var frequencyData = new Uint8Array(200);
+
+  var svgHeight = '300';
+  var svgWidth = '1200';
+  var barPadding = '1';
+
+  function createSvg(parent, height, width) {
+    return d3.select(parent).append('svg').attr('height', height).attr('width', width);
+  }
+
+  var svg = createSvg('body', svgHeight, svgWidth);
+
+  // Create our initial D3 chart.
+  svg.selectAll('rect')
+     .data(frequencyData)
+     .enter()
+     .append('rect')
+     .attr('x', function (d, i) {
+        return i * (svgWidth / frequencyData.length);
+     })
+     .attr('width', svgWidth / frequencyData.length - barPadding);
+
+  // Continuously loop and update chart with frequency data.
+  function renderChart() {
+     requestAnimationFrame(renderChart);
+
+     // Copy frequency data to frequencyData array.
+     analyser.getByteFrequencyData(frequencyData);
+
+     // Update d3 chart with new data.
+     svg.selectAll('rect')
+        .data(frequencyData)
+        .attr('y', function(d) {
+           return svgHeight - d;
+        })
+        .attr('height', function(d) {
+           return d;
+        })
+        .attr('fill', function(d) {
+           return 'rgb(0, 0, ' + d + ')';
+        });
+  }
+
+  // Run the loop
+  renderChart();
+
+});
